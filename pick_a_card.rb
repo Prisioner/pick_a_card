b97833f69fd7c0f78ba5585f6fd11f34d3f47851
Aleksandr Klimenkov <prisioner@mail.ru>
2017-07-03 15:03:16 +0300

    replace suits text with unicode symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 values = %W(2 3 4 5 6 7 8 9 10 J Q K A)
-suits = %W(Diamonds Hearts Clubs Spades)
+suits = %W(\u2666 \u2665 \u2663 \u2660) # Diamonds, Hearts, Clubs, Spades
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
